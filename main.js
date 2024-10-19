3bd469cdfec7067d1324ea272f1655e694bbab9d
Mohamed Gamal <mohamadgamalbusiness@gmail.com>
2024-10-19 21:44:52 +0300

    iphone

---
+++ main.js
@@ -0,0 +1,10 @@
+let iphone = document.querySelector(".phone");
+let container = document.querySelector(".container");
+
+function phones(o) {
+  iphone.src = o;
+}
+
+function color(y) {
+  container.style.backgroundColor = y;
+}
